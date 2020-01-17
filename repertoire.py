0e925c9a2b76a1b263a6fa7dd7c1fb1492022d33
cedricLafumat <cedricLafumat@le-campus-numerique.fr>
2020-01-17 08:55:27 +0100

    Ajout de fonctionnalité de modification, verification présence, erreur si déjà présent, et saisie insensible à la casse

--- repertoire.py
+++ repertoire.py
@@ -3,9 +3,17 @@ repertoire = {
     "tata" : "0706050403"
 }
 
+#repertoire = {}
+
 def saisie_menu():
     saisie_utilisateur = input(
-        "\nQuel est votre choix?\nL pour Lister le répertoire\nA pour Ajouter un contact\nS pour Supprimer un contact\nR pour Rechercher un contact\nChoix : ")
+        "\nQuel est votre choix?\n"
+        "L pour Lister le répertoire\n"
+        "A pour Ajouter un contact\n"
+        "S pour Supprimer un contact\n"
+        "R pour Rechercher un contact\n"
+        "M pour Modifier le numéro d'un contact\nChoix : "
+        ).upper()
     return saisie_utilisateur
 
 def listing(repertoire):
@@ -13,10 +21,18 @@ def listing(repertoire):
         print("Nom : {}\nNuméro de téléphone : {}".format(nom, telephone))
 
 def ajout_contact(repertoire, nom, numero):
-    repertoire[nom] = numero
+    presence_contact = verif_presence(repertoire, nom)
+    if presence_contact == True:
+        print("\n{} est déjà présent dans le répertoire".format(nom))
+    else:
+        repertoire[nom] = numero
 
 def suppression_contact(repertoire, nom):
-    del repertoire[nom]
+    presence_contact = verif_presence(repertoire, nom)
+    if presence_contact == False:
+        print("\n{} n'est pas présent dans le répertoire".format(nom))
+    else:
+        del repertoire[nom]
 
 def recherche_contact(repertoire, nom):
     rech_contact = {}
@@ -25,6 +41,20 @@ def recherche_contact(repertoire, nom):
             ajout_contact(rech_contact, nom_rech, telephone)
     return rech_contact
 
+def modification_contact(repertoire, nom):
+    presence_contact = verif_presence(repertoire, nom)
+    if presence_contact == True:
+        numero_a_modifier = input("Nouveau numéro : ")
+        repertoire[nom] = numero_a_modifier
+
+def verif_presence(repertoire, nom):
+    presence_contact = False
+    for nom_verif in repertoire.keys():
+        if nom_verif == nom:
+            presence_contact = True
+            break
+    return presence_contact
+
 while True:
     saisie_utilisateur = saisie_menu()
     if saisie_utilisateur == "L":
@@ -32,15 +62,19 @@ while True:
         listing(repertoire)
     elif saisie_utilisateur == "A":
         print("Vous avez choisi d'ajouter un contact\n")
-        nom_a_ajouter = input("Ecrivez le nom du contact à ajouter : ")
+        nom_a_ajouter = input("Ecrivez le nom du contact à ajouter : ").lower()
         numero_a_ajouter = input("Ecrivez le numéro du contact : ")
         ajout_contact(repertoire, nom_a_ajouter, numero_a_ajouter)
     elif saisie_utilisateur == "S":
         print("Vous avez choisi de supprimer un contact\n")
-        nom_a_supprimer = input("Quel contact voulez-vous supprimer ? ")
+        nom_a_supprimer = input("Quel contact voulez-vous supprimer ? ").lower()
         suppression_contact(repertoire, nom_a_supprimer)
     elif saisie_utilisateur == "R":
         print("Vous avez choisi de faire une recherche\n")
-        nom_a_rechercher = input("Quel contact recherchez-vous ? ")
+        nom_a_rechercher = input("Quel contact recherchez-vous ? ").lower()
         resultat_recherche = recherche_contact(repertoire, nom_a_rechercher)
         listing(resultat_recherche)
+    elif saisie_utilisateur == "M":
+        print("Vous avez choisi de modifier le numéro d'un contact\n")
+        nom_a_modifier = input("Quel contact voulez-vous modifier? ").lower()
+        modification_contact(repertoire, nom_a_modifier)

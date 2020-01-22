b91f6a6f10a6c7f0c72ad8bef8e43bf8c49d3ec6
cedricLafumat <cedricLafumat@le-campus-numerique.fr>
2020-01-22 10:12:16 +0100

    Création d'un fichier repertoire_utils_dict avec une variable repertoire

---
+++ repertoire_utils_dict.py
@@ -0,0 +1,11 @@
+repertoire = [{
+    "nom" : "toto",
+    "numero" : "0605040302",
+    "mail" : "toto@campus"
+    },{
+    "nom" : "tata",
+    "numero" : "0102030405",
+    "mail" : "tata@campus"
+    }
+]
+
